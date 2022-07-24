1938c16096c24b20d26d1bec9449b163652f1b04
Ceyhun <contact.ceyhun@gmail.com>
2022-07-24 16:09:19 +0400

    initial structure

---
+++ user-management-v1/user-management-v1/UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace user_management_v1.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
