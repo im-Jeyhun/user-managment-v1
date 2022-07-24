02a451cff56ce28b7773a0e4ab4bc0ade3f848f9
Ceyhun <contact.ceyhun@gmail.com>
2022-07-24 16:54:47 +0400

    Completed UI added

--- user-management-v1/user-management-v1/UI/Program.cs
+++ user-management-v1/user-management-v1/UI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using user_management_v1.ApplicationLogic;
 
 namespace user_management_v1.UI
 {
@@ -6,7 +7,39 @@ namespace user_management_v1.UI
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("___________________________________________________________________");
+            Console.WriteLine();
+            Console.WriteLine(" Welcome our application pls enter suitable command for contuniue : ");
+            Console.WriteLine("___________________________________________________________________");
+            Console.WriteLine();
+            Console.WriteLine("/register");
+            Console.WriteLine("/login");
+            Console.WriteLine("/exit");
+            while (true)
+            {
+                Console.Write("Enter suitable command :");
+                string command = Console.ReadLine();
+                if (command == "/register")
+                {
+                    Authentication.Register();
+
+                }
+                else if (command == "/login")
+                {
+
+                    Authentication.Login();
+
+                }
+                else if (command == "/exit")
+                {
+                    Console.WriteLine("Thanks for using our application");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Command not found ...");
+                }
+            }
         }
     }
 }
