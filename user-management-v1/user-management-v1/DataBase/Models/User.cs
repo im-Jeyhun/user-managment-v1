e0d716357a9499f1c3575e07c6ff822cad13a94c
Ceyhun <contact.ceyhun@gmail.com>
2022-07-24 16:15:33 +0400

    User model is completed

---
+++ user-management-v1/user-management-v1/DataBase/Models/User.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_management_v1.DataBase.Models
+{
+    public class User
+    {
+
+        public int Id { get; private set; }
+ 
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public List<Report> Reports { get; set; }
+        protected DateTime RegistrationDate { get; } = DateTime.Now;
+
+      
+        public User(List<Report> report)
+        {
+            Reports = report;
+        }
+
+        //bu sildikden sonra elavet etmek ucun
+        public User(string name, string lastName, string email, string password, int id)
+        {
+          
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            Id = id;       
+
+        }
+        // bu normal elave etmek ucun.
+        public User(string name, string lastName, string email, string password)
+        {
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            Id = UserRepository.IdCounter;
+        }
+
+        public User(string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+
+        public virtual string GetUserInfo()
+        {
+            return $"Istifadeci adi : {Name} , soyadi : {LastName} , emaili : {Email} ";
+
+        }
+
+    }
+}
