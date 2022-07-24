ed578a9ce73641e9f8b295b3d85568f8bd6fd827
Ceyhun <contact.ceyhun@gmail.com>
2022-07-24 16:42:50 +0400

    Completed UserValidation and ReportValidation added to Application logic again

--- user-management-v1/user-management-v1/ApplicationLogic/Validation/UserValidation.cs
+++ user-management-v1/user-management-v1/ApplicationLogic/Validation/UserValidation.cs
@@ -8,7 +8,7 @@ using user_management_v1.DataBase.Repository;
 
 namespace user_management_v1.ApplicationLogic.Validation
 {
-    public class UserValidation
+    public partial class UserValidation
     {
         public static bool IsLengthCorrect(string text, int startLengt, int endLength)
         {
@@ -84,4 +84,128 @@ namespace user_management_v1.ApplicationLogic.Validation
             return false;
         }
     }
+    public partial class UserValidation
+    {
+        public static string GetName()
+        {
+            bool isEceptionValid;
+            string name = null;
+            do
+            {
+
+                try
+                {
+                    Console.Write("Insert Name : ");
+                    name = Console.ReadLine();
+                    if (name == "null")
+                    {
+                        throw new Exception();
+                    }
+                    isEceptionValid = false;
+                }
+                catch (Exception)
+                {
+
+                    isEceptionValid = true;
+                    Console.WriteLine("Seflik var");
+                }
+
+            } while (isEceptionValid || !UserValidation.IsNameValid(name));
+
+
+            return name;
+        }
+        public static string GetLastName()
+        {
+            bool isEceptionValid;
+            string surname = null;
+            do
+            {
+
+                try
+                {
+                    Console.Write("Insert Surname : ");
+                    surname = Console.ReadLine();
+                    if (surname == "null")
+                    {
+                        throw new Exception();
+                    }
+                    isEceptionValid = false;
+                }
+                catch (Exception)
+                {
+
+                    isEceptionValid = true;
+                    Console.WriteLine("Seflik var");
+                }
+
+            } while (isEceptionValid || !UserValidation.IsNameValid(surname));
+
+
+            return surname;
+        }
+        public static string GetEmail()
+        {
+            Console.Write("Insert email : ");
+            string email = Console.ReadLine();
+            while (!UserValidation.IsValidEmail(email) || !UserValidation.IsUserExistsByEmail(email))
+            {
+                Console.Write("Pls enter email again : ");
+                email = Console.ReadLine();
+            }
+            return email;
+        }
+        public static string GetPassword()
+        {
+            string password = null;
+            bool isExceptionValid;
+            do
+            {
+                try
+                {
+
+                    Console.Write("Insert password : ");
+                    password = Console.ReadLine();
+
+                    if (password == "null")
+                    {
+                        throw new Exception();
+                    }
+                    isExceptionValid = false;
+                }
+                catch (Exception)
+                {
+                    isExceptionValid = true;
+                    Console.WriteLine("Xeta var"); ;
+                }
+
+            } while (isExceptionValid || !UserValidation.IsPasswordValid(password));
+
+            string conPass = null;
+            do
+            {
+                try
+                {
+
+                    Console.Write("Insert password again : ");
+                    conPass = Console.ReadLine();
+                    if (conPass == "null")
+                    {
+                        throw new Exception();
+                    }
+                    isExceptionValid = false;
+                }
+                catch
+                {
+                    isExceptionValid = true;
+                    Console.WriteLine("Conifrm pass null exception");
+                }
+
+            } while (isExceptionValid || !UserValidation.IsPasswordSame(password, conPass));
+
+
+            return password;
+        }
+    }
+
 }
