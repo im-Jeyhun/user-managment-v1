29a9d3f5146c6c87ad499a5836de53c5b8570f78
Ceyhun <contact.ceyhun@gmail.com>
2022-07-24 16:31:23 +0400

    Completed UserValidation class added to Validation

---
+++ user-management-v1/user-management-v1/ApplicationLogic/Validation/UserValidation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_management_v1.ApplicationLogic.Validation
+{
+    internal class UserValidation
+    {
+    }
+}
